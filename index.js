01cf04af58760c697d0694befe72cee69a78a5e6
Ibrahimzahra146 <ibrahim.zahra@hotmail.com>
2016-11-30 09:47:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 3000));
 
-// Server frontpage
-app.get('/', function (req, res) {
-    res.send('This is TestBot Server');
-});
-
 // Facebook Webhook
 app.get('/webhook', function (req, res) {
     if (req.query['hub.verify_token'] === 'testbot_verify_token') {
